1abe8e6e715423b9bde2bc62004b142c785d2f48
crswong888 <crswong888@gmail.com>
2020-08-25 12:29:26 -0600

    Did some cleaning up in the rebasing script. This works beautifully now.

--- scripts/rebase_commits.sh
+++ scripts/rebase_commits.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-# This script uses a diff at a specific head to select which lines to copy to the file in the
-# subsequent commit directories. If no sha is specified, it assumes HEAD for comparing diffs
+function printusage {
+    echo "Usage:    ./scripts/rebase_commits.sh <source> <sha>"
+    echo ""
+    echo "    This script does not yet have documentation."
+    echo ""
 
-# remove forward slash from source directory name if provided
+    # This script uses a diff at a specific head to select which lines to copy to the file in the
+    # subsequent commit directories. If no sha is specified, it assumes HEAD for comparing diffs
+}
+
+if [[ "$1" == "-h" || "$1" == "--help" || $# == 0 || $# > 2 ]]; then
+    printusage
+    exit 1
+fi
+
+# remove forward slash from source directory name if provided and check if dir exists
 srcdir="${1%/}"
+if [ ! -d $srcdir ]; then
+  echo "Error: Directory $srcdir does not exist."
+  exit 1
+fi
 
-# determine sha to use for compaing file diffs
+# determine branch head to use for comparing file diffs
 sha=$2
 if [ -z $sha ]; then
   sha="HEAD"
@@ -17,12 +33,76 @@ if [ "$(git cat-file -t $sha)" != "commit" ]; then
   exit 1
 fi
 
-# '/tmp/dstfile' will temporarily store file updates before merging - be sure one isn't lingering
-if [ -f "/tmp/dstfile" ]; then
-  rm /tmp/dstfile
-fi
+# Function for copying lines which don't match a `diff` at $sha from file in $srcdir to $dstdir
+function copydiff {
+  # '/tmp/dstfile' will temporarily store file updates before merging - be sure one isn't lingering
+  if [ -f "/tmp/dstfile" ]; then
+    rm /tmp/dstfile
+  fi
 
-ifs=$IFS # store default internal field separator so we can switch back and forth
+  # define convenience variables (these are the required positional arguments for this func)
+  diff=$1 # diff argument must be the entire string - pass it in "$1" format
+  srcfile=$2
+  dstfile=$3
+
+  # copy only those lines which don't match the retro diff
+  ifs=$IFS # store default internal field separator so we can switch back and forth
+  for i in $(seq 1 $(wc -l < $srcfile))
+  do
+    difftype="None"
+    for d in $diff
+    do
+      # read lower and upper diff line indices (x,x[a|c|d]x,x) from each file into an array
+      IFS="acd" # split string by a, c, or d
+      read -r -a lines <<< $d
+      IFS="," # split strings by commas
+      read -r -a srclines <<< ${lines[0]}
+      read -r -a dstlines <<< ${lines[1]}
+      IFS=$ifs # restore default IFS
+
+      # store lower and upper line diff indices for $srcfile
+      srclower=${srclines[0]}
+      srcupper=${srclines[1]}
+      if [ -z $srcupper ]; then
+        srcupper=$srclower
+      fi
+
+      # lower and upper diff indices for $dstfile
+      dstlower=${dstlines[0]}
+      dstupper=${dstlines[1]}
+      if [ -z $dstupper ]; then
+        dstupper=$dstlower
+      fi
+
+      # determine if current line matches a diff index - get the type of diff if it is
+      if [ $i -ge $srclower ] && [ $i -le $srcupper ]; then
+        difftype=$(echo $d | sed 's/[^acd]*//g') # pipe char to sed - (a)dd, (c)hange, (d)elete
+        break
+      fi
+    done
+
+    # copy lines from $srcfile to '/tmp/dstfile' based on the type of `diff` determined
+    touch /tmp/dstfile
+    if [ $difftype = "None" ]; then
+      sed -n "$i"p $srcfile >> /tmp/dstfile
+    elif [ $difftype = "a" ]; then
+      sed -n "$i"p $srcfile >> /tmp/dstfile
+      for range in $(seq $dstlower $dstupper)
+      do
+        sed -n "$(($(wc -l < /tmp/dstfile) + 1))"p $dstfile >> /tmp/dstfile
+      done
+    elif [ $difftype = "c" ] && [ $i = $srcupper ]; then
+      for range in $(seq $dstlower $dstupper)
+      do
+        sed -n "$(($(wc -l < /tmp/dstfile) + 1))"p $dstfile >> /tmp/dstfile
+      done
+    fi
+  done
+
+  # overwrite $dstfile with the temporary merger file
+  mv /tmp/dstfile $dstfile
+}
+
 for dstdir in step*
 do
   if [ ${dstdir:4:2} -gt ${srcdir:4:2} ]; then
@@ -35,81 +115,17 @@ do
         continue
       fi
 
-      #
+      # Write a temporary file containing the contents of $srcfile at $sha and output a `diff`
       git show $sha:$srcfile > /tmp/srcfile
       diff=$(diff /tmp/srcfile $dstfile | grep '^[1-9]') # pipe to grep to and only output diff codes
       rm /tmp/srcfile
 
-      # temporary
-      if [ -z "$diff" ]; then
-        continue
+      # Invoke function to copy lines from $srcfile to $dstfile which don't match $diff
+      if [ -n "$diff" ]; then
+        echo -n "Merging changes from '$srcfile' into '$dstfile'... "
+        copydiff "$diff" $srcfile $dstfile
+        echo "Done."
       fi
-      # not this ^^^^^
-      # this:
-      #
-      # if [ -n $diff ]; then
-      #   execute the function
-      # fi
-
-      ######################
-      # all of the rest of this can be passed to a function which takes $diff, $srcfile and $dstfile
-      # the function should just remove tmp, its hardly slower and much more secure
-
-      # copy only those lines which don't match the retro diff
-      for i in $(seq 1 $(wc -l < $srcfile))
-      do
-        difftype="None"
-        for d in $diff
-        do
-          # read lower and upper diff line indices (x,x[a|c|d]x,x) from each file into an array
-          IFS="acd" # split string by a, c, or d
-          read -r -a lines <<< $d
-          IFS="," # split strings by commas
-          read -r -a srclines <<< ${lines[0]}
-          read -r -a dstlines <<< ${lines[1]}
-          IFS=$ifs # restore default IFS
-
-          # store lower and upper line diff indices for $srcfile
-          srclower=${srclines[0]}
-          srcupper=${srclines[1]}
-          if [ -z $srcupper ]; then
-            srcupper=$srclower
-          fi
-
-          # lower and upper diff indices for $dstfile
-          dstlower=${dstlines[0]}
-          dstupper=${dstlines[1]}
-          if [ -z $dstupper ]; then
-            dstupper=$dstlower
-          fi
-
-          # determine if current line matches a diff index - get the type of diff if it is
-          if [ $i -ge $srclower ] && [ $i -le $srcupper ]; then
-            difftype=$(echo $d | sed 's/[^acd]*//g') # pipe char to sed - (a)dd, (c)hange, (d)elete
-            break
-          fi
-        done
-
-        #
-        touch /tmp/dstfile
-        if [ $difftype = "None" ]; then
-          sed -n "$i"p $srcfile >> /tmp/dstfile
-        elif [ $difftype = "a" ]; then
-          sed -n "$i"p $srcfile >> /tmp/dstfile
-          for range in $(seq $dstlower $dstupper)
-          do
-            sed -n "$(($(wc -l < /tmp/dstfile) + 1))"p $dstfile >> /tmp/dstfile
-          done
-        elif [ $difftype = "c" ] && [ $i = $srcupper ]; then
-          for range in $(seq $dstlower $dstupper)
-          do
-            sed -n "$(($(wc -l < /tmp/dstfile) + 1))"p $dstfile >> /tmp/dstfile
-          done
-        fi
-      done
-
-      # overwrite $dstfile with the temporary merger file
-      mv /tmp/dstfile $dstfile
     done
   fi
 done
