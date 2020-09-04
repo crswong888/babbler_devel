a9b9feb879d091fe246438ef101d5f9fc149b58e
crswong888 <crswong888@gmail.com>
2020-09-03 20:11:55 -0600

    improvements to test_moose.sh script

--- scripts/test_moose.sh
+++ scripts/test_moose.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
 
-# Test all Babbler applications inside the MOOSE directory
-#
-# How to exit if test or compilation fails? (Need to do the same in update_babbler.sh)
-for file in ../moose/tutorials/tutorial01_app_development/step*
+### Test all Babbler applications inside the MOOSE directory
+
+failed=false # initialize variable to track wether compilation, testing, etc., of babbler failed
+for file in ~/projects/moose/tutorials/tutorial01_app_development/step*
 do
   echo "Entering directory: $file"
   cd $file
-
   make clean
-  make -j4
-  ./run_tests -j4
 
-  echo "Leaving directory: $file"
-  cd ../../../../babbler_devel
+  echo -n "Compiling Babbler... "
+  make clean &> /tmp/babbler_devel_out
+  make -j4 &> /tmp/babbler_devel_out # make the -j4 optional input
+  if [ $? -eq 0 ]; then
+    echo "Done."
+  else
+    echo ""
+    cat /tmp/babbler_devel_out
+    echo "Error: Babbler failed to compile."
+    failed=true
+    break
+  fi
+
+  echo -n "Running test harness... "
+  ./run_tests -j4 &> /tmp/babbler_devel_out
+  if [ $? -eq 0 ]; then
+    echo "Done."
+  else
+    echo ""
+    cat /tmp/babbler_devel_out
+    failed=true
+    break
+  fi
+
+  for inputfile in $(git ls-files *.i)
+  do
+    echo -n "Executing $inputfile... "
+    ./babbler-opt -i $inputfile &> /tmp/babbler_devel_out
+    if [ $? -eq 0 ]; then
+      echo "Done."
+    else # moose error report will be output
+      echo ""
+      cat /tmp/babbler_devel_out
+      failed=true
+      break 2
+    fi
+  done
 done
+rm /tmp/babbler_devel_out
+
+echo ""
+if ! $failed; then
+  echo "Testing of Babbler applications in MOOSE completed succesfully."
+else
+  echo "Testing of Babbler applications in MOOSE failed. Please check output for error reports."
+fi
