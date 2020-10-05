167a38c049e1b577ea0dfb54ba422dd9123ae097
crswong888 <crswong888@gmail.com>
2020-10-05 16:42:42 -0600

    started the material property object for darcy pressure coefficients

---
+++ step07_mat_props/include/materials/PackedColumn.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Material.h"
+
+/**
+ * Computes the permeability of a porous medimum made up of packed steel balls of a specified radius
+ * in accordance with Pamuk and Özdemir (2012). Also provides a specified dynamic viscosity of the
+ * fluid in the medium.
+ */
