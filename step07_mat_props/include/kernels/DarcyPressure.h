5731eb18831aeaacf347f4dfe66269eef427aab2
crswong888 <crswong888@gmail.com>
2020-11-16 14:51:57 -0700

    declared _permeability and _viscosity as references in step 6

--- step07_mat_props/include/kernels/DarcyPressure.h
+++ step07_mat_props/include/kernels/DarcyPressure.h
@@ -18,6 +18,6 @@ protected:
   virtual ADRealVectorValue precomputeQpResidual() override;
 
   /// The variables which hold the value for K and mu
-  const Real _permeability;
-  const Real _viscosity;
+  const Real & _permeability;
+  const Real & _viscosity;
 };
