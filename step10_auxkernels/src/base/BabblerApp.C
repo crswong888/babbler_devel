c6e69c4f5a0d683ba24d475e473b8360f9a00c4d
crswong888 <crswong888@gmail.com>
2021-02-28 14:36:55 -0700

    commit directory for step 10 - velocity aux kernel

---
+++ step10_auxkernels/src/base/BabblerApp.C
@@ -0,0 +1,56 @@
+#include "BabblerApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+BabblerApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+
+  // Do not use legacy DirichletBC, that is, set DirichletBC default for preset = true
+  params.set<bool>("use_legacy_dirichlet_bc") = false;
+
+  // Do not use legacy material output, i.e., output properties on INITIAL as well as TIMESTEP_END
+  params.set<bool>("use_legacy_material_output") = false;
+
+  return params;
+}
+
+BabblerApp::BabblerApp(InputParameters parameters) : MooseApp(parameters)
+{
+  BabblerApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+BabblerApp::~BabblerApp() {}
+
+void
+BabblerApp::registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  ModulesApp::registerAll(f, af, s);
+  Registry::registerObjectsTo(f, {"BabblerApp"});
+  Registry::registerActionsTo(af, {"BabblerApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+BabblerApp::registerApps()
+{
+  registerApp(BabblerApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+BabblerApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  BabblerApp::registerAll(f, af, s);
+}
+extern "C" void
+BabblerApp__registerApps()
+{
+  BabblerApp::registerApps();
+}
